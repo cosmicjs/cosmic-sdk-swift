c263334520dadc5296b508b41cc6e9381fabff71
Karl Koch <karl@kejk.tech>
2023-09-20 11:02:38 +0100

    fix: Error handling on Find

--- Sources/cosmic-sdk/CosmicEndpointProvider.swift
+++ Sources/cosmic-sdk/CosmicEndpointProvider.swift
@@ -27,11 +27,20 @@ public struct CosmicEndpointProvider {
     }
     
     func getPath(api: API, id: String?, bucket: String, type: String, read_key: String, write_key: String?, props: String?, limit: String?, title: String?, slug: String?, content: String?, metadata: [String: AnyCodable]?) -> String {
+        let write_key = "&write_key=\(String(describing: write_key))"
+        let props = "&props=\(String(describing: props))"
+        let limit = "&limit=\(String(describing: limit))"
+        if let id = id {
+            return id
+        } else {
+            print("Error: Please provide an object id")
+        }
+        
         switch source {
         case .cosmic:
             switch api {
             case .find:
-                return "/v3/buckets/\(bucket)/objects?pretty=true&query=%7B%22type%22%3A%22\(type)%22%7D&read_key=\(read_key)&write_key=\(String(describing: write_key))&props=\(String(describing: props))&limit=\(String(describing: limit))"
+                return "/v3/buckets/\(bucket)/objects?pretty=true&query=%7B%22type%22%3A%22\(type)%22%7D&read_key=\(read_key)\(write_key)\(props)\(limit)"
             case .findOne:
                 return "/v3/buckets/\(bucket)/objects/\(id!)"
             case .insertOne:
